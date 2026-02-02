867b62f7d5123e5e5bef112f7b4935735560413a
Samuel <samueelrs979@gmail.com>
2026-02-02 16:48:05 -0300

    changes made on all the project

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  retries: 0,
+  use: {
+    headless: true
+  },
+  tsconfig: './tsconfig.tests.json'
+});
